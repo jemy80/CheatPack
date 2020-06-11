f9198ba3d21f95d0cd02c93169f69e14da9039c1
Naveq <naveq.yt@gmail.com>
2020-06-11 12:55:05 +0200

    0.0.4 - Server Update Note: versions <0.0.4 will not work.

--- server/brainly/limitbypass.js
+++
@@ -1,6 +0,0 @@
-/**
- * Brainly homework answers view limit bypass by Naveq (https://github.com/Naveq)
- * @author Naveq
- */
-localStorage.clear();
-document.getElementsByClassName('js-page-wrapper')[0].click();
--- server/quizizz/inject.js
+++
@@ -1,1 +0,0 @@
-// There aren't anything now, but maybe I'll want to put some scripts here in the future.
